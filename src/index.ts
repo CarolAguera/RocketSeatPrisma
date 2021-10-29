9bca16852d2cc554c98066323cf2fa31383ec700
carolinaaguera <carolaguerabr@gmail.com>
2021-10-29 12:58:00 -0300

    Buscando todos os usuarios

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,23 @@ import { PrismaClient } from ".prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.create({
-    data: {
-      name: "Carolina Aguera",
-      email: "carolaguerabr@gmail.com",
-      groups: {
-        create: {
-          title: "Grupo-1"
-        }
-      }
-    }
+  // const newUser = await prisma.user.create({
+  //   data: {
+  //     name: "Carolina Aguera",
+  //     email: "carolaguerabr@gmail.com",
+  //     groups: {
+  //       create: {
+  //         title: "Grupo-1"
+  //       }
+  //     }
+  //   }
+  // })
+  // console.log('Created new user: ', newUser)
+
+  const allUsers = await prisma.user.findMany({
+    include: { groups: true },
   })
+  console.log('All users: ')
+  console.dir(allUsers, { depth: null })
 }
-main();
+main().catch((e) => console.error(e));
